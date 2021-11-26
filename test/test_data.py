61448f305fab0c8b5c5feb1b26c2885085e3de55
Gilad Kutiel <gilad.kutiel@gmail.com>
2021-11-26 12:23:39 +0200

    test_data

--- test/test_data.py
+++ test/test_data.py
@@ -2,20 +2,18 @@ from datetime import datetime
 from wabot.parser import Msg
 
 
-def test_to_sessions():
-    from wabot.data import to_sessions
+def test_MsgDataset():
+    from wabot.data import MsgDataset
+    msgs = [Msg(datetime(2020, 1, i+1), 'sender', 'text') for i in range(10)]
+    ds = MsgDataset(msgs)
 
-    def msg(h, m):
-        return Msg(
-            time=datetime(2018, 1, 1, h, m),
-            sender='Bob',
-            text='')
+    assert len(ds) == 10
+    assert ds[0] == msgs[0]
 
-    msgs = [msg(0, 0), msg(0, 1), msg(0, 2), msg(0, 3), msg(0, 4), msg(0, 5)]
-    assert len(list(to_sessions(msgs))) == 1
 
-    msgs = [msg(0, 0), msg(0, 1), msg(0, 2), msg(0, 20), msg(0, 21), msg(0, 50)]
-    sessions = list(to_sessions(msgs))
-    assert len(sessions[0]) == 3
-    assert len(sessions[1]) == 2
-    assert len(sessions[2]) == 1
+def test_msg_data_loader():
+    from wabot.data import msg_data_loader
+    msgs = [Msg(datetime(2020, 1, i+1), 'sender', 'text') for i in range(10)]
+    dl = msg_data_loader(msgs)
+
+    assert len(dl) == 10
