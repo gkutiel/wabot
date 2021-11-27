eca566fbc01bdd353771a03aeaecca186d598f0a
Gilad Kutiel <gilad.kutiel@gmail.com>
2021-11-27 11:25:34 +0200

    0.6293

--- wabot/Params.py
+++ wabot/Params.py
@@ -3,9 +3,9 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class Params:
-    hidden_size: int = 32
+    hidden_size: int = 18
     batch_size: int = 5_000
-    lr: float = 0.05
+    lr: float = 0.1
     min_tokens: int = 10
     max_tokens: int = 20
     lexicon_size: int = 15_000
